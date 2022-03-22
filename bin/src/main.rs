42cd3ba3e09cfdd7f6ead524c8fb464426dbef65
SaadDAHMANI <tahatatakor7@gmail.com>
2022-03-22 17:21:38 +0100

    add bin output
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,3 +1,25 @@
+
+use sefar::benchmarks::functions::F1;
+use sefar::sequential_algos::eo::EO;
+use sefar::sequential_algos::eo::EOparams;
+use sefar::core::eoa::EOA;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, sefar !");
+
+    eo_f1_test1();
 }
+
+fn eo_f1_test1(){
+    let mut settings : EOparams = EOparams::default();
+    //settings.population_size = 20;
+    //settings.max_iterations = 500; 
+
+    let mut fo = F1 {};
+
+    let mut eo : EO<F1> = EO::new(&settings, &mut fo);
+    
+    let result = eo.run();
+       
+    println!("EO result : \n {:?}", result);      
+ }
--- src/lib.rs
+++ src/lib.rs
@@ -23,15 +23,18 @@ mod tests {
 
     #[test]
     fn eo_f1_test1(){
-        let mut settings : EOparams = EOparams::default();
-        settings.population_size = 20;
-        settings.max_iterations = 500; 
+        let settings : EOparams = EOparams::default();
+        //settings.population_size = 20;
+        //settings.max_iterations = 500; 
 
         let mut fo = F1 {};
 
         let mut eo : EO<F1> = EO::new(&settings, &mut fo);
         
         let result = eo.run();
+
+        let bestfit = result.best_fitness.unwrap();
+
 
         assert_eq!(result.best_fitness, Some(0.0001));
         
