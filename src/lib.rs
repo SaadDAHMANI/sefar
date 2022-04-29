1e0835d95bb50746d990d26a3bdd1a66560a83df
SaadDAHMANI <tahatatakor7@gmail.com>
2022-04-29 17:01:05 +0100

    test PSO for Sphere obj.fn
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,10 @@ mod common;
 #[cfg(test)]
 mod tests {
     use crate::benchmarks::functions::Sphere;
+    use crate::core::eoa::EOA;
     use crate::sequential_algos::eo::EO;
     use crate::sequential_algos::eo::EOparams;
-    use crate::core::eoa::EOA;
+    use crate::sequential_algos::pso::*;
     //use super::*;
 
     #[test]
@@ -31,4 +32,22 @@ mod tests {
         assert_ne!(result.best_fitness, None);
         
      }
+
+     #[test]
+     fn pso_f1_test1(){
+         let settings : PSOparams = PSOparams::default();
+         //settings.population_size = 20;
+         //settings.max_iterations = 500; 
+     
+         let mut fo = Sphere {};
+ 
+         let mut pso : PSO<Sphere> = PSO::new(&settings, &mut fo);
+         
+         let result = pso.run();
+ 
+         //let bestfit = result.best_fitness.unwrap();
+ 
+         assert_ne!(result.best_fitness, None);
+         
+      } 
 }
